e0ebcd0b5e882cc6c75038aa5d67b90e647bdc40
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-15 11:45:57 -0700

    (test) Added usability test/examples.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 #![deny(warnings)]
 
 //! Defines the `Convertible` trait, for safely attempting to convert
-//! types from a raw representation.
+//! types from a raw representation at runtime.
 
 /// Mixin trait for all types that defines the `to` method,
-/// which allows checked conversion to another type that
+/// which allows checked, runtime conversion to another type that
 /// is marked as Convertible from that type.
 pub trait Raw {
     /// Checked, runtime conversion of a type to a type that
@@ -25,5 +25,41 @@ impl<T> Raw for T {
 pub trait Convertible<From> {
     /// Try to create an instance of `Self` from `From`.
     fn convert(&From) -> Option<Self>;
+}
+
+// Mostly just usability tests.
+#[cfg(test)]
+mod test {
+    use super::{Convertible, Raw};
+
+    struct Raw {
+        name: &'static str
+    }
+
+    #[deriving(PartialEq, Show)]
+    enum NotRaw {
+        First, Second, Third
+    }
+
+    impl Convertible<Raw> for NotRaw {
+        fn convert(raw: &Raw) -> Option<NotRaw> {
+            match raw.name {
+                "first" => Some(First),
+                "second" => Some(Second),
+                "third" => Some(Third),
+                _ => None
+            }
+        }
+    }
+
+    #[test] fn test_conversion() {
+        let first = Raw { name: "first" };
+        let second = Raw { name: "second" };
+        let malformed = Raw { name: "malformed" };
+
+        assert_eq!(first.to::<NotRaw>(), Some(First));
+        assert_eq!(second.to::<NotRaw>(), Some(Second));
+        assert_eq!(malformed.to::<NotRaw>(), None);
+    }
 }
 
