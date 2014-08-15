f98dbeac162558042dd208de8afeec4a936321ee
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-15 11:28:46 -0700

    (feat) Convertible and Raw.

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,28 @@
 #![deny(missing_doc)]
 #![deny(warnings)]
 
-//! Crate comment goes here
+//! Defines the `Convertible` trait, for safely attempting to convert
+//! types from a raw representation.
+
+/// Mixin trait for all types that defines the `to` method,
+/// which allows checked conversion to another type that
+/// is marked as Convertible from that type.
+pub trait Raw {
+    /// Checked, runtime conversion of a type to a type that
+    /// is convertible from `Self`.
+    fn to<C: Convertible<Self>>(&self) -> Option<C>;
+}
+
+impl<T> Raw for T {
+    fn to<C: Convertible<T>>(&self) -> Option<C> {
+        Convertible::convert(self)
+    }
+}
+
+/// Defines the convert method, which allows checked construction
+/// of a type from a type it is convertible from.
+pub trait Convertible<From> {
+    /// Try to create an instance of `Self` from `From`.
+    fn convert(&From) -> Option<Self>;
+}
 
