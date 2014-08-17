8079e0bf4ebfcf3ebba69ec439e3ceb1b38665dd
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-17 02:07:55 -0700

    (feat) Added Coercible for by-value conversions.

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,43 @@ pub trait Raw {
     /// Checked, runtime conversion of a type to a type that
     /// is convertible from `Self`.
     fn to<C: Convertible<Self>>(&self) -> Option<C>;
+
+    /// Checked, runtime coercion which consumes the passed-in
+    /// type. Uses the Coercible trait to do the conversion.
+    ///
+    /// self is returned inside the Err variant if the coercion fails.
+    fn coerce<C: Coercible<Self>>(self) -> Result<C, Self>;
 }
 
 impl<T> Raw for T {
     fn to<C: Convertible<T>>(&self) -> Option<C> {
         Convertible::convert(self)
     }
+
+    fn coerce<C: Coercible<T>>(self) -> Result<C, T> {
+        Coercible::coerce(self)
+    }
 }
 
 /// Defines the convert method, which allows checked construction
 /// of a type from a type it is convertible from.
+///
+///
 pub trait Convertible<From> {
     /// Try to create an instance of `Self` from `From`.
     fn convert(&From) -> Option<Self>;
+}
+
+/// Defines the coerce method, which allows checked conversion
+/// of a type from a type that it is coercible from.
+///
+/// This is like the Convertible trait, except the method
+/// takes From by-value.
+pub trait Coercible<From> {
+    /// Try to convert an instance of `From` to `Self`
+    ///
+    /// This should return Err(from) if the coercion fails.
+    fn coerce(From) -> Result<Self, From>;
 }
 
 // Mostly just usability tests.
